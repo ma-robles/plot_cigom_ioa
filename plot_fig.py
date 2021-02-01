a162de038b1ae3bf3a713234fd21c64a11c3ea51
miguel angel lap <miguel.robles@atmosfera.unam.mx>
2021-02-01 03:54:09 -0600

    added input file

--- plot_fig.py
+++ plot_fig.py
@@ -10,6 +10,9 @@ import numpy as np
 import netCDF4 as nc
 from map_plots import map_pcolor, map_quiver, add_quiverPlot
 import json
+import argparse as ap
+from configparser import ConfigParser
+import shlex
 
 def parse_name(figname):
     '''
@@ -81,9 +84,9 @@ def parse_name(figname):
 
 def create_tree(struct_val):
     path='Figuras/'
-    path+=struct_val['tipo']+'/'
-    path+=struct_val['var']+'/'
-    path+=struct_val['stat']+'/'
+    path+=struct_val.tipo+'/'
+    path+=struct_val.var_name+'/'
+    path+=struct_val.stat+'/'
     os.makedirs(path,exist_ok=True)
     return(path)
 
@@ -93,13 +96,8 @@ def parse_units(units):
     else:
         return units
 
-def plot(filename, figname):
-    info=parse_name(figname)
-    print('info:',info)
-    if info==1:
-        print('***¡Sintaxis de nombre incorrecta!***')
-        exit(1)
-    path=os.path.join(create_tree(info),figname)
+def plot(args):
+    path=os.path.join(create_tree(args),args.figname)
     print(path)
     #define names in netCDF file
     var_names={
@@ -127,49 +125,39 @@ def plot(filename, figname):
             11:'Noviembre',
             12:'Diciembre',
             }
-    with open('config_plot.json','r') as ifile:
-        data_cfg=json.load(ifile)
-    if info['tipo']=='clim':
-        per='0'
-    elif info['tipo']=='mensual':
-        per=str(info['mes'])
-    cfg=data_cfg[info['var']][per][str(info['depth'])][info['stat']]
-    print('cmap:',cfg)
-    title='Climatología'
-    #if info['tipo']=='mensual':
-    #    title+='de '+month[info['mes']]
-    #elif info['tipo']=='estacional':
-    #    title+='de '+info['estacion']
-    title+=' de '+info['var']
-    title+=' ('+info['stat']+')'
-    if info['depth']!=-1:
-        title+=' a '+str(info['depth'])+' m '
-    no_depth= False
-    with nc.Dataset(filename, 'r') as root:
-        try:
+    title=args.stat
+    title=title[0].upper()+title[1:]
+    if args.stat=='media':
+        title=''
+    if args.tipo=='mensual':
+        title+=' mensual de la '
+    elif args.tipo=='clim':
+        title+='Climatología (1992-2012) de la '
+    title+=args.var_name
+    if args.depth!=None:
+        title+=' a '+str(args.depth)+' m '
+    if args.var_alias!=None:
+        var_name=args.var_alias
+    else:
+        var_name=args.var_name
+    with nc.Dataset(args.filename, 'r') as root:
+        if args.depth!=None:
             z=root.variables['Depth'][:]
-        except:
-            no_depth = True
-        if no_depth == False:
-            idepth=np.argwhere(z==info['depth'])[0][0]
-            var=root.variables[var_names[info['var']]][0][idepth]
+            idepth=np.argwhere(z==args.depth)[0][0]
+            var=root.variables[var_name][0][idepth]
         else:
-            var=root.variables[var_names[info['var']]][0]
-        try:
-            lon=root.variables['Longitude'][:]
-        except:
-            lon=root.variables['longitude'][:]
-
-        try:
-            lat=root.variables['Latitude'][:]
-        except:
-            lat=root.variables['latitude'][:]
+            var=root.variables[var_name][0]
+        lon=root.variables[args.long][:]
+        lat=root.variables[args.lat][:]
         #time=nc.num2date(root.variables['time'][:], root.variables['time'].units)
-        units=root.variables[var_names[info['var']]].units
+        if args.units==None:
+            units=root.variables[args.units].units
+        else:
+            units=args.units
 
-    units=parse_units(units)
     units='['+units+']'
     title+=units
+    title+=' Modelo '+args.modelo
     print(title)
 
     ax, figure = map_pcolor(lon, lat, var,
@@ -178,12 +166,64 @@ def plot(filename, figname):
                 'x':[-98,-95,-92,-89,-86,-83,-80,-77],
                 'y':[18,20,22,24,26,28,30,32],
                 },
-            cmap=cfg['cmap'],
-            plot_land=True, vmin=cfg['vmin'], vmax=cfg['vmax'])
-    if info['tipo']=='mensual':
-        ax.annotate(month[info['mes']],xy=(0.02,0.94), xycoords='axes fraction')
+            cmap=args.cmap,
+            plot_land=True,
+            vmin=args.vmin,
+            vmax=args.vmax)
+    if args.tipo=='mensual':
+        ax.annotate(month[args.mes],xy=(0.02,0.94), xycoords='axes fraction')
     figure.savefig(path, bbox_inches='tight', dpi=200)
 
-figname=sys.argv[1]
-ifilename=sys.argv[2]
-plot(ifilename, figname)
+#parsing args
+parser= ap.ArgumentParser()
+group=parser.add_mutually_exclusive_group()
+parser.add_argument("--tipo", help="Tipo de figura",
+        choices=["clim","mensual","estacional"],
+        )
+group.add_argument("--var_name", help="Nombre de la variable",
+        choices=["capa-mezcla","nivel-mar", "temperatura", "salinidad", "viento",
+        "nitratos","carbono","clorofila"],
+        )
+parser.add_argument("--var_alias", help="Nombre de la variable en el archivo")
+parser.add_argument("--stat", help="Operación estadística aplicada",
+        choices=["media","desviacion-estandar","maximos","minimos","promedio"],
+        )
+parser.add_argument("--filename", help="Nombre del archivo")
+parser.add_argument("--ID", help="Clave del modelo",
+        default="gom-unam-hycom-ioa-gom-phy-025",
+        )
+parser.add_argument("--depth", type=int, help="Profundidad en m")
+parser.add_argument("--mes", type=int, help="Número del mes")
+parser.add_argument("--root", help="Path de la carpeta raíz")
+parser.add_argument("--cmap", help="Paleta de la barra de colores")
+parser.add_argument("--vmin", type=float, help="Valor mínimo en la barra de colores")
+parser.add_argument("--vmax", type=float, help="Valor máximo en la barra de colores")
+parser.add_argument("--title", help="Título en la gráfica")
+parser.add_argument("--xcomp", help="Componente X en variables vectoriales")
+parser.add_argument("--xfilename", help="Archivo con la componente X")
+parser.add_argument("--ycomp", help="Componente Y en variables vectoriales")
+parser.add_argument("--yfilename", help="Archivo con la componente Y")
+parser.add_argument("--lat", default="latitude", help="Nombre de la variable de latitud")
+parser.add_argument("--long", default="longitude",help="Nombre de la variable de longitud")
+parser.add_argument("--units", help="Especifica unidades de la variable")
+parser.add_argument("--modelo", help="Especifica el modelo usado")
+group.add_argument('-i',"--input", help="Archivo(s) de entrada", dest='config_file')
+
+#parser.add_argument("--figname", help="Nombre de la figura")
+args=parser.parse_args()
+if args.config_file!=None:
+    print('Reading:',args.config_file)
+    file_parser=ConfigParser()
+    file_parser.read(args.config_file)
+    for section_name in file_parser.sections():
+        print('  Variable:', section_name)
+        arg_list=shlex.split('--var_name='+section_name+' '+\
+                file_parser.get(section_name,'options'))
+        args=parser.parse_args(arg_list)
+figname='_'.join([args.ID,args.tipo,args.var_name,args.stat])
+if args.depth!=None:
+    figname+='_'+'{:04d}'.format(args.depth)+'m'
+if args.mes!=None:
+    figname+='_M'+'{:02d}'.format(args.mes)
+args.figname=figname
+plot(args)
